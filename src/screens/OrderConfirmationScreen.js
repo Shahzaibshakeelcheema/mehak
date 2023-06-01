65e25742f931a4a4a29e2cce05eddb4b14a18410
AG-M002 <shahzaibshakeelcheema@gmail.com>
2023-06-01 21:16:47 +0500

    Compeeled Task [200~ Start an E-commerce website with good design and you can use local storage because you don't have the backend yet.

--- src/screens/OrderConfirmationScreen.js
+++ src/screens/OrderConfirmationScreen.js
@@ -1,35 +1,98 @@
-// OrderConfirmationScreen.js
 import React from 'react';
 import { useSelector } from 'react-redux';
-import{Row,Col}from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap';
+
 const OrderConfirmationScreen = () => {
   const order = useSelector((state) => state.cartList.order);
-//console.log(typeof order ,'sdfghjklkjhgfxcvjkl')
-  const [name,address,items]=order;
-debugger
+
   if (!order) {
     return <div>No order details available</div>;
   }
-console.log(order.name,'.name')
-console.log(order[0])
+
+  const [name, address, items] = order;
+
+  // Calculate total quantity and total price
+  let totalQuantity = 0;
+  let totalPrice = 0;
+
+  items.forEach((item) => {
+    totalQuantity+= Number(item.qty);
+    totalPrice += item.qty * item.price;
+  });
 
   return (
     <div className="container">
-    <Row className='border mt-5'>
-    <Row className=''>      <h1>Order Confirmation</h1></Row>
-    <Row className=''><Col>Order ID: </Col><Col>      <h3> 1</h3></Col></Row>
-    <Row className=''><Col>Name: </Col><Col>      <h4>{order[0]}</h4></Col></Row>
-    <Row className=''><Col>Address: </Col><Col>      <h4>{order[1]}</h4></Col></Row>
-    
-    
-      <h4>Items:</h4>
-      <div>
-        {order[2]?.map((item) => (
-          <div key={item.id}>
-           <Row className='border mt-2'> <Col>{item.title} </Col><Col> Quantity: {item.qty}</Col></Row>
-          </div>
-        ))}
-      </div>
+      <Row className="border mt-5 table table-hover">
+        <Row>
+          <h1>Order Confirmation</h1>
+        </Row>
+        <Row className="">
+          <Col>Order ID:</Col>
+          <Col>
+            <h3>1</h3>
+          </Col>
+        </Row>
+        <Row className="">
+          <Col>Name:</Col>
+          <Col>
+            <h4>{name}</h4>
+          </Col>
+        </Row>
+        <Row className="">
+          <Col>Address:</Col>
+          <Col>
+            <h4>{address}</h4>
+          </Col>
+        </Row>
+
+        <h4>Items:</h4>
+        <div>
+          <Row>
+            <Col>
+              <h6>
+                <b>Product</b>
+              </h6>
+            </Col>
+            <Col>
+              <h6>
+                <b>Quantity</b>
+              </h6>
+            </Col>
+            <Col>
+              <h6>
+                <b>Price</b>
+              </h6>
+              
+            </Col>
+          </Row>
+
+          {items.map((item) => (
+            <div key={item.id}>
+              <Row className="border mt-2">
+                <Col>{item.title}</Col>
+                <Col>{item.qty}</Col>
+                <Col>${item.qty*item.price}</Col>
+              </Row>
+            </div>
+          ))}
+        </div>
+        <Row>
+          <Col>
+            <h6>
+              <b>Total</b>
+            </h6>
+          </Col>
+          <Col> 
+            <h6>Units: 
+              <b>{totalQuantity}</b>
+            </h6>
+          </Col>
+          <Col>
+            <h6>
+              <b>${totalPrice.toFixed(2)}</b>
+            </h6>
+          </Col>
+        </Row>
       </Row>
     </div>
   );
